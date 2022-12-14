6dd7ec92e5139d5b4d864bdf8f3ff6a0984c4fc7
VinhDang88 <vincedang720@gmail.com>
2022-12-14 11:51:59 -0500

    VD Cinemas Controller

---
+++ eTickets/Controllers/CinemasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
